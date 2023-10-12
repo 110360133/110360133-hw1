19b27b5adc1afafc83ea9ebb39bc36f21ddd59af
unknown <jeff920422@gmail.com>
2023-10-12 23:42:00 +0800

    110360133

---
+++ src/homework1.java
@@ -0,0 +1,116 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class homework1 
+{
+	public static void main(String[] args)throws IOException
+	{
+		//System.out.print("歡迎使用Java!");
+		//System.out.print("歡迎使用Java!吧");
+	
+		//System.out.println("歡迎使用Java!");
+		//System.out.println("歡迎使用Java!吧");
+		
+		//System.out.println('A');
+		//System.out.println("歡迎使用Java!");
+		//System.out.println(123);
+		
+		//stem.out.println("顯示出反斜線:\\");
+		//System.out.println("顯示出單引號\'");
+		
+		//System.out.println("八進位數101的字元是\101");
+		//System.out.println("十六進位數0061的字元是\u0061");
+		
+		//int num;
+		
+		//num = 3 ;
+		
+		//System.out.println("變數num的值是"+ num);
+		
+		//num = 5;
+		
+		//System.out.println("更新變數num的值");
+		//System.out.println("變數num更新後的值是"+ num);
+		
+		//int num1,num2;
+		
+		//num1=3;
+		
+		//System.out.println("變數num1的值是:" + num1);
+		
+		//num2 = num1;
+		
+		//System.out.println("將變數num1指定到變數num2之中");
+		//System.out.println("變數num2的值是:"+ num2);
+		
+		/*System.out.print("請輸入一個整數:");
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		String str1 = br1.readLine();
+		int num = Integer.parseInt(str1);
+		System.out.println("你輸入的數字是:" + num);
+		System.out.print("請輸入字串:");
+		
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+		String str2 = br2.readLine();
+		System.out.println("剛剛輸入的字串是:" + str2);*/
+		
+		/*System.out.println("1+2等於" + (1 + 2));
+		System.out.println("3*4等於" + (3 * 4));
+		
+		int num1 = 2;
+		int num2 = 3;
+		int sum = num1 + num2;
+		System.out.println("變數num1的值是" + num1);
+		System.out.println("變數num2的值是" + num2);
+		System.out.println("num1+num2的值是" + sum);
+		
+		num1 = num1 + 1;
+		System.out.println("變數num1的值加1之後是" + num1);*/
+		
+		/*int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println("num1和num2的各種運算:");
+		System.out.println("num1+num2等於" + (num1 + num2));
+		System.out.println("num1-num2等於" + (num1 - num2));
+		System.out.println("num1*num2等於" + (num1 * num2));
+		System.out.println("num1/num2等於" + (num1 / num2));
+		System.out.println("num1%num2等於" + (num1 % num2));
+		
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		b = a++;
+		c = ++a;
+		
+		System.out.println("因為是在指定值之後才遞增，所以b的值為" + b);
+		System.out.println("因為是在遞增後才指定值，所以c的值為" + c);*/
+		
+		/*double dnum = 160.5;
+		System.out.println("身高是" + dnum + "公分");
+		System.out.println("指定給int型態的變數");
+		int inum = (int)dnum;
+		System.out.println("身高是" + inum + "公分");*/
+		
+		int d = 2;
+		double pi = 3.14;
+		
+		System.out.println("直徑是" + d + "公分的圓");
+		System.out.println("其圓周為" + (d * pi) + "公分");
+		
+		int num1 = 5;
+		int num2 = 4;
+		
+		double div1 = num1 / num2;
+		double div2 = (double)num1 / (double)num2;
+		
+		System.out.println("5/4等於" + div1);
+		System.out.println("5/4等於" + div2);
+		
+		
+
+	}
+
+}
